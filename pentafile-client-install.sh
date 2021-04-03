30d77448dec7449c4abe8c268c1a45e2695ea9f7
Reynaldo Claros <reyiclaros@gmail.com>
2021-04-02 20:05:21 -0500

    Update script

--- pentafile-client-install.sh
+++ pentafile-client-install.sh
@@ -4,7 +4,7 @@ wget -O pf https://cdn.pentafilestore.com/0761a7bf95f238ebe9006ff6f9521335
 
 mkdir $HOME/.pentafile
 
-mv pf $HOME/.pentafile
+mv pf $HOME/.pentafile/
 
 chmod 777 $HOME/.pentafile/pf
 
