149075119941ad8c26b6332c0d46bfa2830c1892
Reynaldo Claros <reyiclaros@gmail.com>
2021-04-02 22:51:04 -0500

    Update Content

--- pentafile-client-install.sh
+++ pentafile-client-install.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
 cd /tmp
-wget -O pf https://cdn.pentafilestore.com/0761a7bf95f238ebe9006ff6f9521335
-mkdir -p $HOME/.pentafile
-mv pf $HOME/.pentafile/
-chmod 777 $HOME/.pentafile/pf
-export PATH=$PATH:$HOME/.pentafile/pf
+echo "checking..."
+URL=https://cdn.pentafilestore.com/0761a7bf95f238ebe9006ff6f9521335
+LOCAL="$HOME"/.pentafile
+PF="$HOME"/.pentafile/pf
+wget -O pf -q $URL
+mkdir -p $LOCAL
+mv pf $LOCAL
+chmod u+x $PF
+echo "PENTAFILE=$HOME/.pentafile 
+export PENTAFILE" >> ~/.bash_profile
+source ~/.bash_profile
+echo "Pentafile client install succefull"
